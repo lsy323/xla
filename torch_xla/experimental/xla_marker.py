c8502de26278af5d0e25e69899b3c1395b07a12b
Siyuan Liu <lsiyuan@google.coim>
2023-11-14 18:14:56 +0000

    add xla_marker file

---
+++ torch_xla/experimental/xla_marker.py
@@ -0,0 +1,23 @@
+import torch
+import torch_xla
+from torch.library import Library, impl
+
+xla_pattern_marking_lib = Library("xla_pattern_marking", "DEF")
+
+xla_pattern_marking_lib.define("tag_tensor(Tensor x, str tag) -> Tensor")
+
+
+@impl(xla_pattern_marking_lib, "tag_tensor", "CompositeExplicitAutograd")
+def tag_tensor(x, tag):
+  return x
+
+
+@impl(xla_pattern_marking_lib, "tag_tensor", "XLA")
+def tag_tensor(x, tag):
+  torch_xla._XLAC._xla_add_tag(x, tag)
+  return x
+
+
+@impl(xla_pattern_marking_lib, "tag_tensor", "Meta")
+def tag_tensor(x, tag):
+  return torch.empty_like(x)
