578cc08af50890cb63cd4e310beb911141e7b2ee
Siyuan Liu <lsiyuan@google.coim>
2023-11-28 01:57:58 +0000

    add unit tests

--- test/stablehlo/test_mark_pattern.py
+++ test/stablehlo/test_mark_pattern.py
@@ -1,222 +1,129 @@
-import torch_xla
-import torch_xla.core.xla_model as xm
-import torch
+import sys
 import unittest
-import numpy as np
-import torch_xla.experimental
-from torch_xla.experimental import tagging_utils
-from torch_xla.stablehlo import exported_program_to_stablehlo
 
-# def dual_output(x, y):
-#     return (x + y, x - y)
-
-# # Pattern to match in the exported graph.
-# def dual_output_pattern(x, y):
-#     return dual_output(x, y)
-
-# class M(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#     def forward(self, x, y):
-#         exp_x = torch.exp(x)
-#         exp_y = torch.exp(y)
-#         out1, out2 = dual_output(exp_x, exp_y)
-#         out = torch.log(out1) + torch.log(out2)
-#         return out
-
-# m = M().eval()
-# args = (torch.rand(2, 3), torch.rand(2, 3))
-# pattern_args = (torch.rand(2, 3), torch.rand(2, 3))
-# model_ep = torch._export.export(m, args)
-
-# print("check exported module")
-# model_ep.graph_module.graph.print_tabular()
-# model_ep = tagging_utils.mark_pattern(model_ep, dual_output_pattern, pattern_args)
-# print("check exported module after tagged")
-# model_ep.graph_module.graph.print_tabular()
-
-# args = tuple(i.to(xm.xla_device()) for i in args if hasattr(i, "to"))
-# res = model_ep(*args)
-
-# stablehlo = xm.get_stablehlo([res])
-# print(stablehlo)
-
-# #########
-# def my_softmax(x, y):
-#     out = x + y
-#     out = torch.abs(out)
-#     out = torch.nn.LogSoftmax(dim=1)(out)
-#     out = torch.nn.ReLU6()(out)
-#     out = torch.log(out)
-#     return out
-
-# def my_inverse(x):
-#     return torch.div(1, x) + x
-
-# # Pattern to match in the exported graph.
-# def pattern(x, y):
-#     return my_softmax(x, y)
-
-# # Pattern to match in the exported graph.
-# def inverse_pattern(x):
-#     return my_inverse(x)
-
-# class M2(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#     def forward(self, x, y, z):
-#         exp_x = torch.exp(x)
-#         exp_y = torch.exp(y)
-#         exp_z = torch.exp(z)
-#         out1 = my_softmax(exp_x, exp_y)
-#         out2 = my_softmax(exp_y, exp_z)
-#         out1 = torch.abs(out1)
-#         out2 = torch.abs(out2)
-#         out1 = my_inverse(out1)
-#         out2 = my_inverse(out2)
-#         out = out1 + out2
-#         return out
-
-# m = M2().eval()
-# args = (torch.rand(2, 3), torch.rand(2, 3), torch.rand(2, 3))
-# model_ep = torch._export.export(m, args)
-# pattern_args = (torch.rand(2, 3), torch.rand(2, 3))
-# inverse_pattern_args = (torch.rand(2, 3),)
-# # Wrap the exported graph with tagging callbacks
-# model_ep = tagging_utils.mark_pattern(model_ep, pattern, pattern_args)
-
-# model_ep = tagging_utils.mark_pattern(model_ep, inverse_pattern, inverse_pattern_args)
-
-# args = tuple(i.to(xm.xla_device()) for i in args if hasattr(i, "to"))
-# res = model_ep(*args)
-
-# stablehlo = xm.get_stablehlo([res])
-
-# print(stablehlo)
-
-######## Module pattern ##########
-# class M(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.fc = torch.nn.Linear(3,5)
-
-#     def forward(self, x):
-#         x = torch.abs(x)
-#         x = self.fc(x)
-#         x = torch.log(x)
-#         return x
-
-# class P(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.fc = torch.nn.Linear(3,5)
-
-#     def forward(self, x):
-#         return self.fc(x)
-
-# m = M().eval()
-# args = (torch.rand(10, 3),)
-# model_ep = torch.export.export(m, args)
-# model_ep.graph_module.graph.print_tabular()
-
-# mp = P().eval()
-# pattern_args = (torch.rand(10, 3),)
-
-#############
-# def my_softmax(x, y):
-#     out = x + y
-#     out = torch.abs(out)
-#     out = torch.nn.LogSoftmax(dim=1)(out)
-#     out = torch.nn.ReLU6()(out)
-#     out = torch.log(out)
-#     return out
-
-# class M(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#     def forward(self, x, y):
-#         exp_x = torch.exp(x)
-#         exp_y = torch.exp(y)
-#         out = my_softmax(exp_x, exp_y)
-#         out = torch.abs(out)
-#         return out
-
-# # Pattern to match in the exported graph.
-# def pattern(x, y):
-#     return my_softmax(x, y)
-
-# m = M().eval()
-# args = (torch.rand(2, 3), torch.rand(2, 3))
-# pattern_args = (torch.rand(2, 3), torch.rand(2, 3))
-
-# model_ep = torch.export.export(m, args)
-# model_ep.graph_module.graph.print_tabular()
-# model_ep = tagging_utils.mark_pattern(model_ep, pattern, pattern_args)
-# model_ep.graph_module.graph.print_tabular()
-# # Trace with PyTorch/XLA
-# args = tuple(i.to(xm.xla_device()) for i in args if hasattr(i, "to"))
-# res = model_ep(*args)
-
-# Export StableHLO after PyTorch/XLA Tracing
-# stablehlo = xm.get_stablehlo([res])
-# print(stablehlo)
-
-# hlo = xm.get_hlo([res])
-# print(hlo)
+import torch
+import torch_xla.core.xla_model as xm
+import torch_xla.experimental.xla_marker
+from torch.utils import _pytree as pytree
 
 
-#############
-# Pattern to match in the exported graph.
-def log_softmax_pattern(x, dim):
-  return torch.nn.LogSoftmax(dim=dim)(x)
+class XlaMarkPatternTest(unittest.TestCase):
+
+  def run_func_get_stablehlo(self, f, input_args):
+
+    device = xm.xla_device()
+    input_args = pytree.tree_map_only(torch.Tensor, lambda x: x.to(device=device),
+                                    input_args)
+    out = f(*input_args)
+    if isinstance(out, tuple):
+      out = list(out)
+    else:
+      out = [out]
+    stablehlo = xm.get_stablehlo(out)
+    return stablehlo
+
+  def test_basic(self):
+    def f(x):
+      x = x + 1
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p", 0, 0, True)
+      x = x + 2
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p", 0, 0, False)
+      return x
+
+    input_args = (torch.randn(5),)
+    stablehlo = self.run_func_get_stablehlo(f, input_args)
+    self.assertEqual(stablehlo.count("@stablehlo.composite"), 1)
+    self.assertTrue('{attributes = {}, name = "p"}' in stablehlo)
+
+  def test_sdpa_pattern(self):
+    import torch.nn.functional as F
+    class M(torch.nn.Module):
+
+      def __init__(self):
+        super().__init__()
+
+      def forward(self, x, y):
+        q, k, v = x.split(128, dim=-2)
+        q = torch.ops.xla_pattern_marking.mark_tensor(q, "sdpa", pos=0, id=0, is_input=True)
+        k = torch.ops.xla_pattern_marking.mark_tensor(k, "sdpa", pos=1, id=0, is_input=True)
+        v = torch.ops.xla_pattern_marking.mark_tensor(v, "sdpa", pos=2, id=0, is_input=True)
+        attn_out = F.scaled_dot_product_attention(q, k, v, scale=0.25)
+        attn_out = torch.ops.xla_pattern_marking.mark_tensor(attn_out, "sdpa", pos=0, id=0, is_input=False, attr={"scale":0.25})
+        q, k, v = y.split(128, dim=-2)
+        q = torch.ops.xla_pattern_marking.mark_tensor(q, "sdpa", pos=0, id=1, is_input=True)
+        k = torch.ops.xla_pattern_marking.mark_tensor(k, "sdpa", pos=1, id=1, is_input=True)
+        v = torch.ops.xla_pattern_marking.mark_tensor(v, "sdpa", pos=2, id=1, is_input=True)
+        attn_out2 = F.scaled_dot_product_attention(q, k, v, scale=4)
+        attn_out2 = torch.ops.xla_pattern_marking.mark_tensor(attn_out2, "sdpa", pos=0, id=0, is_input=False, attr={"scale":2})
+        return attn_out, attn_out2
+
+    input_args = (torch.randn((32, 8, 384, 64)), torch.randn((32, 8, 384, 64)))
+    stablehlo = self.run_func_get_stablehlo(M(), input_args)
+    self.assertEqual(stablehlo.count("@stablehlo.composite"), 2)
+    self.assertTrue('{attributes = {scale = 2.500000e-01 : f32}, name = "sdpa"}}' in stablehlo)
+    self.assertTrue('{attributes = {scale = 2 : i64}, name = "sdpa"}}' in stablehlo)
+
+  def test_multiple_input(self):
+    def f(x, y):
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p", 0, 0, True)
+      y = torch.ops.xla_pattern_marking.mark_tensor(y, "p", 1, 0, True)
+      out = x + y
+      out = out * x * y
+      out = torch.ops.xla_pattern_marking.mark_tensor(out, "p", 0, 0, False)
+      return out
+
+    input_args = (torch.ones(5), torch.ones(5))
+    stablehlo = self.run_func_get_stablehlo(f, input_args)
+    self.assertEqual(stablehlo.count("@stablehlo.composite"), 1)
+    self.assertTrue('{attributes = {}, name = "p"}' in stablehlo)
 
 
-class M(torch.nn.Module):
+  @unittest.skip("Multiple outputs patterns are not supported now.")
+  def test_multiple_output(self):
+    def f(x, y):
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p", 0, 0, True)
+      y = torch.ops.xla_pattern_marking.mark_tensor(y, "p", 1, 0, True)
+      out1 = x + y
+      out2 = x * y
+      out1 = torch.ops.xla_pattern_marking.mark_tensor(out1, "p", 0, 0, False)
+      out2 = torch.ops.xla_pattern_marking.mark_tensor(out2, "p", 1, 0, False)
+      return out1, out2
 
-  def __init__(self):
-    super().__init__()
+    input_args = (torch.ones(5), torch.ones(5))
+    stablehlo = self.run_func_get_stablehlo(f, input_args)
 
-  def forward(self, x):
-    r = x
-    for idx in range(5):
-      r = torch.nn.LogSoftmax(dim=idx % 2)(r) * x
-    return r
+  @unittest.skip("Nested pattern is not supported now.")
+  def test_nested_pattern(self):
+    def f(x):
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_outter", 0, 0, True)
+      x = x + 1
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_inner", 0, 0, True)
+      x = x + 1
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_inner", 0, 0, False)
+      x = x * 2
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_outter", 0, 0, False)
+      return x
+
+    input_args = (torch.ones(5),)
+    stablehlo = self.run_func_get_stablehlo(f, input_args)
+
+  @unittest.skip("Nested pattern is not supported now.")
+  def test_tangent_output(self):
+    # Special case of nested pattern, outputs don't have dependencies.
+    def f(x):
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_outter", 0, 0, True)
+      x = x + 1
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_inner", 0, 0, True)
+      x = x + 1
+      y = x - 1
+      x = torch.ops.xla_pattern_marking.mark_tensor(x, "p_inner", 0, 0, False)
+      y = torch.ops.xla_pattern_marking.mark_tensor(y, "p_outter", 0, 0, False)
+      return x, y
+
+    input_args = (torch.ones(5),)
+    stablehlo = self.run_func_get_stablehlo(f, input_args)
 
 
-m = M().eval()
-args = (torch.rand(200, 200),)
-model_ep = torch.export.export(m, args)
-
-model_ep = tagging_utils.mark_pattern(
-    pattern_name="log_softmax_pattern",
-    exported_ep=model_ep,
-    pattern=log_softmax_pattern,
-    pattern_args=(torch.rand(10, 10, 10), 1),
-    const_attr_trackers=[
-        tagging_utils.ConstAttrTracker(
-            "dim", pattern_arg_pos=1).track(0).track(1).track(2),
-    ])
-
-
-out = model_ep(torch.rand(200, 200).to(xm.xla_device()))
-# hlo_content = torch_xla._XLAC._get_xla_tensors_hlo([out])
-# print(hlo_content)
-stablehlo = xm.get_stablehlo([out])
-print(stablehlo)
-# shlo_bundle = exported_program_to_stablehlo(model_ep)
-# print(shlo_bundle.get_stablehlo_text("forward"))
-
-# model_ep.run_decompositions()
-# print("check here")
-# model_ep.graph_module.graph.print_tabular()
-# args = tuple(i.to(xm.xla_device()) for i in args if hasattr(i, "to"))
-# res = model_ep(*args)
-
-# stablehlo = xm.get_stablehlo([res])
-# print(stablehlo)
-
-# stablehlo_bytecode = xm.get_stablehlo_bytecode([res])
-# print(stablehlo_bytecode)
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
